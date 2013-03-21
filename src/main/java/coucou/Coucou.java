186194c8981ba54bcc06e7992713c11c4147fb49
Hedi Hedda <hedda.hedi@etu.unice.fr>
2013-03-21 11:40:51 +0100

    requete json

--- src/main/java/coucou/Coucou.java
+++ src/main/java/coucou/Coucou.java
@@ -6,15 +6,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.codehaus.jettyson.json.JSONObject;
+
+
 
 @Path("/coucou")
 public class Coucou {
 	
 	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response repondre() {
-		return Response.ok("OK").build();
+		JSONObject data = new JSONObject("../ressources/mock_data.json");
+		return Response.ok(data.toString()).build();
 	}
 	
 	
