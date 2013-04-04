50978d67a078eaaca5ed61226850d0caa9a722b7
pahahenki <swanmanson@hotmail.fr>
2013-04-04 13:42:42 +0200

    repository organsation
    separated js, css and html

---
+++ src/main/webapp/js/treemap.js
@@ -0,0 +1,78 @@
+var margin = {top: 100, right: 10, bottom: 10, left: 10},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var color = d3.scale.category20c();
+
+var treemap = d3.layout.treemap()
+	.children(function(d, depth) { return depth===3 ? null : d.children; })
+    .size([width, height])
+    .sticky(true)
+    .value(function(d) { return d.size? d.size : 3000 ; })
+    .padding(1);
+    
+    
+    
+    //ajout une div a  body avec les style en question
+
+var div = d3.select("body").append("div")
+	.attr("classe", "treemap")
+    .style("position", "relative")
+    .style("width", (width + margin.left + margin.right) + "px")
+    .style("height", (height + margin.top + margin.bottom) + "px")
+    .style("left", margin.left + "px")
+    .style("top", margin.top + "px");
+    
+    
+
+var root= treemap.nodes(g5k);
+console.log(root[0].name);
+   
+	
+  var node = div.datum(root)
+  	.selectAll(".node")
+      .data(treemap.nodes)
+      .enter().append("div")
+      .attr("class", "node")
+    .attr("id", function(d){ var name="";
+          var node = d;
+          while(node.parent){
+          if (name ==""){
+            name= node.name
+          }
+          else{
+            name= node.name+ "." + name ;
+            node=node.parent;
+          }
+          }
+          name= node.name +"." + name;
+          return name;
+})
+      .call(position)
+    .style("background", function(d) {;return d.children ? color(d.name) : null; })
+      .text(function(d) { return d.depth===2 ? d.name : null });
+      
+      
+
+
+
+  /*d3.selectAll("input").on("change", function change() {
+    var value = this.value === "count"
+        ? function() { return 1; }
+        : function(d) { return d.size; };
+
+    node
+        .data(treemap.value(value).nodes)
+      .transition()
+        .duration(1500)
+        .call(position);
+  });*/
+
+
+function position() {
+  this.style("left", function(d) { return d.x + "px"; })
+      .style("top", function(d) { return d.y + "px"; })
+      .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+      .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+}
+
